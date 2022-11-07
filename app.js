1c88fe48cf814ae8a772f28ea99ba6115e3a3585
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2022-11-07 12:06:47 +0000

    [mouseover mouserout]

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 $(function(){
-  $('.box1').slideDown(3000,function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'  
-    }).slideUp();
-   });
- });
-  
-  
- 
+  $('.box1').mouseover(function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
+});
+
