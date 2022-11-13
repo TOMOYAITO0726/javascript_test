ae108f52b17d2ea6cc9da99fbb4dae5ef81ee31c
TOMOYAITO0726 <tomoyaito199507265724@gmail.com>
2022-11-13 06:53:36 +0000

    [debug]

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-// （API_KEY には、"取得したAPIキー" を記述）
-const API_KEY = "ebaed39fcca4626e89dbd14b21fc2df9";
-
-$(function(){
-  $('#btn').on('click', function() {
-    // 入力された都市名でWebAPIに天気情報をリクエスト
-    $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "ebaed39fcca4626e89dbd14b21fc2df9",
-      dataType : 'jsonp',
-    }).done(function (data){
-      //通信成功
-      // 位置
-      $('#place').text(data.name);
-      // 最高気温
-      $('#temp_max').text(data.main.temp_max);
-      // 最低気温
-      $('#temp_min').text(data.main.temp_min);
-      //湿度
-      $('#humidity').text(data.main.humidity);
-      //風速
-      $('#speed').text(data.wind.speed);
-      // 天気
-      $('#weather').text(data.weather[0].main);
-      // 天気アイコン
-      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-      $('img').attr("alt",data.weather[0].main);
-    }).fail(function (data) {
-      //通信失敗
-      alert('通信に失敗しました。');      });
+$(function () {
+  // Enterを押せなくする
+  $(document).on("keypress", "input:not(.allow_submit)", function (event) {
+    return event.which !== 13;
   });
-});
+
+  trials = 0;
+  ran = Math.round(Math.random() * 9);
+
+  $('.jquery-form [name=input-number]').change(function () {
+
+    // 試行回数
+    trials++;
+    $('.trials').html(trial);
+
+    const VAL_NUMBER = $(this).val();
+    let val = Number(VAL_NUMBER) || 0; // NaNのとき0にする
+    const RESULT = Math.abs(val - ran); //数字がどのぐらい離れているか
+
+    //判定処理
+    if (val > 0 || val < 9) {
+      $('.result-text').html("範囲内で入力してね！");
+    } else if (RESULT == 0) {
+      $('.result-text').html("正解!");
+    } else if (RESULT <= 2) {
+      $('.result-text').html("惜しいね！");
+    } else {
+      $('.result-text').html("違うよ！");
+    }
+  });
+})
